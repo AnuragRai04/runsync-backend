3f673148f98d6768eb850449b4678489f0dc9079
ANURAG <anuragrai412@gmail.com>
2026-05-08 01:23:24 +0530

    fix: update redis config to accept cloud REDIS_URL

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,12 @@
 const Redis = require("ioredis");
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: process.env.REDIS_PORT || 6379,
-});
+// If Render provides a REDIS_URL, use it! Otherwise, use localhost for local testing.
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({
+      host: process.env.REDIS_HOST || "127.0.0.1",
+      port: process.env.REDIS_PORT || 6379,
+    });
 
 redis.on("connect", () => {
   console.log("✅ Redis Connected Successfully");
